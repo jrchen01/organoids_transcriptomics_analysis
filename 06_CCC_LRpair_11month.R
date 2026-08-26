405deb5882c841e7911b1a0b4e7c8d220fa4d375
Jiarui Chen <168141662+jrchen01@users.noreply.github.com>
2026-08-25 23:21:59 -0400

    Update 06_CCC_LRpair_11month.R

--- 06_CCC_LRpair_11month.R
+++ 06_CCC_LRpair_11month.R
@@ -1,6 +1,6 @@
 # CellChat supplementary - LR pair / pathway level differential signaling, 11-month organoids
 ## GABA-B and Glutamate pathways for Inhibitory 3 and Inhibitory 4
-## needs the subtype-level CellChat objects from 05_CCC_11month.R
+## input: subtype-level CellChat objects from 05_CCC_11month.R
 
 library(Seurat)
 library(CellChat)
